f3637a479849591a9d2d6396e312692211bc43bc
kuvill <lenyavr@gmail.com>
2025-12-15 12:44:06 +0200

    5 laba

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/kuvil/Project/University/labs/embedded/3
+# Install script for directory: /home/kuvil/Project/University/labs/embedded/stm32_pm2025
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/kuvil/Project/University/labs/embedded/3/install_local_manifest.txt"
+  file(WRITE "/home/kuvil/Project/University/labs/embedded/stm32_pm2025/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/kuvil/Project/University/labs/embedded/3/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/kuvil/Project/University/labs/embedded/stm32_pm2025/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- main.c
+++ main.c
@@ -1,62 +1,67 @@
 #include <stdint.h>
 #include <stm32f10x.h>
 
-#include "delay.h"
+volatile uint32_t blink_period = 4096; 
+uint8_t button_state = 0;
+
+void TIM2_IRQHandler(void) {
+    if (TIM2->SR & TIM_SR_UIF) { 
+        GPIOC->ODR ^= GPIO_ODR_ODR13; 
+        TIM2->SR &= ~TIM_SR_UIF; 
+    }
+}
+
+void set_timer_period(uint32_t ms) {
+    TIM2->PSC = 1023; 
+    TIM2->ARR = ms - 1; 
+    TIM2->CR1 |= TIM_CR1_CEN; 
+}
 
 [[noreturn]] int main(void) {
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-
     
     GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);
-    GPIOC->CRH |= GPIO_CRH_MODE13_0;
+    GPIOC->CRH |= GPIO_CRH_MODE13_0; 
 
-    
     GPIOC->CRH &= ~(GPIO_CRH_CNF14 | GPIO_CRH_MODE14);
-    GPIOC->CRH |= GPIO_CRH_CNF14_0;
+    GPIOC->CRH |= GPIO_CRH_CNF14_0; 
 
     GPIOC->CRH &= ~(GPIO_CRH_CNF15 | GPIO_CRH_MODE15);
-    GPIOC->CRH |= GPIO_CRH_CNF15_0;
+    GPIOC->CRH |= GPIO_CRH_CNF15_0; 
 
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
     
-    uint32_t blink_frequency = 1;
-
-    // in ms
-    uint32_t blink_period = 1000;
-    uint8_t button_state = 0;
+    set_timer_period(blink_period);
+    
+    TIM2->DIER |= TIM_DIER_UIE; 
+    NVIC_EnableIRQ(TIM2_IRQn); 
 
     while (1) {
         
-        GPIOC->ODR ^= GPIO_ODR_ODR13;
-        delay_ms(blink_period);
-
-        
         if (!(GPIOC->IDR & GPIO_IDR_IDR14)) {
-            if ((button_state & 0x01) == 0) { 
-                blink_frequency *= 2;
-                if (blink_frequency > 64) blink_frequency = 64;
-                blink_period = 1000 / blink_frequency;
+            if ((button_state & 0x01) == 0) {
+                blink_period = blink_period > 50 ? blink_period / 2 : blink_period; 
+                set_timer_period(blink_period); 
                 button_state |= 0x01;
 
-                // while (!(GPIOC->IDR & GPIO_IDR_IDR14));
-                delay_ms(50);
+                
+                for (volatile int i = 0; i < 100000; i++);
             }
         } else {
             button_state &= ~0x01;
         }
 
-        
         if (!(GPIOC->IDR & GPIO_IDR_IDR15)) {
-            if ((button_state & 0x02) == 0) { 
-                blink_frequency /= 2; 
-                if (blink_frequency < 1) blink_frequency = 1; 
-                blink_period = 1000 / blink_frequency; 
-                button_state |= 0x02; 
+            if ((button_state & 0x02) == 0) {
+                blink_period = blink_period < 2000 ? blink_period * 2 : blink_period; 
+                set_timer_period(blink_period); 
+                button_state |= 0x02;
 
-                // while (!(GPIOC->IDR & GPIO_IDR_IDR15));
-                delay_ms(50);
+                
+                for (volatile int i = 0; i < 100000; i++);
             }
         } else {
-            button_state &= ~0x02; 
+            button_state &= ~0x02;
         }
     }
 }
